c71b2b0bbf726fe4276e60ee9d6a7282f8112d08
ngcoming <iyadhalmlaoui@gmail.com>
2022-07-11 00:11:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-6pkrs6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-6pkrs6)
---
+++ index.ts
@@ -0,0 +1,12 @@
+let age: number;
+age = parseInt ( prompt ( " put ur age "));
+if ( age >=1 || age <=3 ) {
+  console.log("أنت رضيع")
+} else if (age >=4 || age <= 12 ) {
+  console.log("انت طفل")
+} else if( age >=13 || age <=21) {
+  console.log("انت شاب")
+} else if ( age >= 22 ) {
+  console.log(" انت رجل")
+} 
+console.log("end");
